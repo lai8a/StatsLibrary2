4e40b166a86305f04f8acdcad40a71432a045e63
lai8a <khanl1@go.stockton.edu>
2023-12-06 23:44:52 -0500

    Create Salter.java

---
+++ Salter.java
@@ -0,0 +1,17 @@
+//------------------------------------------------------------------
+// Laiba Khan, CSCI3327 Probability and Applied Statistics
+// Chapter 1:
+// StatsLibrary: Mean Median Mode and Standard Deviation Functions
+//--------------------------------------------------------------------public class Salter {
+import java.util.Random;
+
+    //program to read csv values
+    public static Double Salt(double[] csvfile){
+        //read through file for y values and get a i/+ num
+        //from it
+        
+
+    //main method to test different salt values
+    //have high and low data points
+    public static void main(String[] )}
+}
