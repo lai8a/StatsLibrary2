a8235e00678e361f1b8cedde194b1308c580fd05
lai8a <khanl1@go.stockton.edu>
2023-12-09 22:44:23 -0500

    Program Plotter completely done!

--- Program Plotter/ProgramPlotter.java
+++ Program Plotter/ProgramPlotter.java
@@ -6,7 +6,7 @@
  --------------------------------------------------------------------
  */
 
-//We are going to plot f(x) = e + .5x^(sin(x))
+//We are going to plot f(x) = e + x^(sin(x))
 
 import java.io.BufferedWriter; 
 import java.io.FileWriter;
@@ -17,32 +17,30 @@ import java.lang.Math;
 
 public class ProgramPlotter{
 
-    public static List<double[]> ResultsInRange(int start, int stop){
-        int x;
+    public static List<double[]> ResultsInRange(int start, int stop, double step){
+        double x;
         double y; //y is going to be our result
-        double e = Math.E;
         ArrayList<double[]> results = new ArrayList<double[]>();
 
-        while (start <= stop){
-            x = start;
-            y = e + Math.pow(.5, Math.sin(x)); //f(x) = e + .5x^(sin(x))
+        for (x = start; x <= stop; x += step){
+            double expon = Math.sin(x);
+            y = Math.E + Math.pow(x, expon); //f(x) = e + x^(sin(x))
 
             //add x and y (results) into a list of lists
             results.add(new double[] {x, y});
-            
-            start++;
         }
         return results;
     }
+
     //Utilized a reference for help: 
     //Utilized Gennadiy Shevtsov's answer at
     //https://www.quora.com/How-can-data-be-written-in-a-CSV-file-using-Java
 
-    public static void CSVWriter(List<double[]> results){
+    public static void CSVWriter(List<double[]> resultslist, String filename){
         //write onto csv file
 
-        int len = results.size(); //get length of arraylist
-        String csvfile = "functionResults.csv"; //initialize name of csv file
+        int len = resultslist.size(); //get length of arraylist
+        String csvfile = filename + ".csv"; //initialize name of csv file
         int i = 0; //counter for later
 
         //write each line onto csv file with loop
@@ -51,14 +49,14 @@ public class ProgramPlotter{
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
             //writing the headers:
-            bufferedWriter.write("f(x) = e + .5x^(sin(x))");
+            bufferedWriter.write("f(x) = e + x^(sin(x))");
             bufferedWriter.newLine();
 
             bufferedWriter.write("Input,Output");
             bufferedWriter.newLine();
 
             while (i < len){
-                double[] values = results.get(i);
+                double[] values = resultslist.get(i);
                 bufferedWriter.write(values[0] + "," + values[1]); // Separate values with a comma
                 bufferedWriter.newLine();
 
@@ -72,9 +70,20 @@ public class ProgramPlotter{
         }
     }
     public static void main(String[] args) {
-        //Test range 1 - 100
-        List<double[]> results = ResultsInRange(1, 100); // Adjust the range as needed
-        CSVWriter(results);
+        //Test range 1 - 100, with .5 step
+        List<double[]> results = ResultsInRange(1, 100, 0.5); 
+        CSVWriter(results, "functionResults-Config1");
+        System.out.println("CSV file written successfully!");
+
+        //Test range 1 - 1000, with 1 step
+        List<double[]> results2 = ResultsInRange(1, 1000, 1); 
+        CSVWriter(results2, "functionResults-Config2");
+        System.out.println("CSV file written successfully!");
+
+        //Test range -100 - 1, with 2 step
+        //this one outputs mostly NaN because the results are undefined
+        List<double[]> results3 = ResultsInRange(-100, 1, 2); 
+        CSVWriter(results3, "functionResults-Config3");
         System.out.println("CSV file written successfully!");
     }
 }
